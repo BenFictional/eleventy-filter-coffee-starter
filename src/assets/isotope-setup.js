305b866508da92dd64a4d13f7105b1f4bb321b73
Benjamin Andrew <beandrew@gmail.com>
2021-10-20 11:10:56 -0400

    functioning Jquery isotope

--- src/assets/isotope-setup.js
+++ src/assets/isotope-setup.js
@@ -11,7 +11,6 @@ $(document).ready(function() {
 
   // init Isotope
   var $grid = $('.timeline').isotope({
-    itemSelector: '.project'
     itemSelector: '.project',
     percentPosition: true,
     layoutMode: 'cellsByRow',
