134f84dbeb05919a05851ec02662b80c34ca9b2a
Benjamin Andrew <beandrew@gmail.com>
2022-03-29 13:48:10 -0400

    Fixed cloudinary fetch url

--- .eleventy.js
+++ .eleventy.js
@@ -58,11 +58,11 @@ module.exports = (config) => {
 
     // If the shortcode defines aspect as 'p' » set ratio="portrait", otherwise it's "landscape"
       var ratio = (aspect =="p") ? "portrait":"landscape";
-      var imageSize = (size =="small") ? "w_480":"";
+      var imageSize = (size =="small") ? ",w_480":"";
 
       // Only run in production mode
       if (process.env.PROD === 'true') {
-          return `<img src="https://res.cloudinary.com/benjand/image/fetch/q_auto,f_auto,${imageSize}/https://benjaminandrew.net/images/${path}" alt="${alt}" class="${ ratio }">`
+          return `<img src="https://res.cloudinary.com/benjand/image/fetch/q_auto,f_auto${imageSize}/https://benjaminandrew.net/images/${path}" alt="${alt}" class="${ ratio }">`
       }
 
       // Local dev
